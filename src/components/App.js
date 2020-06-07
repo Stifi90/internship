d3898b8ffabca020fc3149bcb643d9d88aa2debb
Stifi90 <konrad.styrylski@gmail.com>
2020-06-07 20:37:18 +0200

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -5,29 +5,51 @@ import DataCollector from "./DataCollector";
 import axios from "axios";
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
+  const [companies, setCompanies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(10);
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    async function getData() {
       setLoading(true);
+
       const res = await axios.get(
         "https://recruitment.hal.skygate.io/companies"
       );
-      setPosts(res.data);
-      setLoading(false);
-    };
 
-    fetchPosts();
+      res.data
+        .map(async (company) => {
+          const incomes = await axios.get(
+            `https://recruitment.hal.skygate.io/incomes/${company.id}`
+          );
+
+          company.incomes = incomes.data.incomes;
+          company.total = Number(
+            incomes.data.incomes
+              .map((income) => (income.value = Number(income.value)))
+              .reduce((partial_sum, a) => partial_sum + a, 0)
+              .toFixed(2)
+          );
+        })
+        .sort(function (a, b) {
+          return a.total - b.total;
+        });
+
+      console.log(res.data);
+
+      setCompanies(res.data);
+      setLoading(false);
+    }
+
+    getData();
   }, []);
 
   // const data = posts.map((post) => <Income id={post.id} />);
 
-  const test = posts.map((post) => DataCollector(post.id));
-  console.log(posts);
-  console.log(test);
+  // const test = posts.map((post) => DataCollector(post.id));
+  // console.log(posts);
+  // console.log(test);
 
   return <div>działa</div>;
 };
